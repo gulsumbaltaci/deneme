af5530bec18838efc06414502830eaab3824d8df
Gülsüm Baltacı <gulsummbaltaci.55@gmail.com>
2023-12-02 16:36:10 +0200

    vvv

--- src/main/java/deneme/GitAdd.java
+++ src/main/java/deneme/GitAdd.java
@@ -4,5 +4,6 @@ public class GitAdd {
     public static void main(String[] args) {
         System.out.println("deneme basarili");
         System.out.println("v1");
+        System.out.println("deneme");
     }
 }
